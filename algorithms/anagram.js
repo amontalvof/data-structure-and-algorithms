e48acd288d26ce8f644c3fc9f7e28cf9c4791572
Andy Montalvo <59586600+amontalvof@users.noreply.github.com>
2021-03-02 16:18:24 -0600

    Update anagram.js

--- algorithms/anagram.js
+++ algorithms/anagram.js
@@ -1,3 +1,10 @@
+/*
+Any time you have multiple pieces of data, and you need to compare them. 
+- anagrams
+- one array is equal to another array squared each individual piece 
+- to tell if numbers consist of the same digits just in a different order
+*/
+
 function validAnagram(first, second) {
   if (first.length !== second.length) {
     return false;
