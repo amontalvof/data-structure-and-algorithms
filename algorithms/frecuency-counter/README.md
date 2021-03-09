dafa0f3e68eb5a6238876ce92e6165b8b2b607f5
amontalvof <amontalvof92@gmail.com>
2021-03-09 10:03:19 -0600

    feat: added README in frequency counter

---
+++ algorithms/frecuency-counter/README.md
@@ -0,0 +1,5 @@
+## Any time you have multiple pieces of data, and you need to compare them.
+
+-   anagrams
+-   one array is equal to another array squared each individual piece
+-   to tell if numbers consist of the same digits just in a different order
--- algorithms/frecuency-counter/anagram.js
+++ algorithms/frecuency-counter/anagram.js
@@ -1,36 +1,29 @@
-/*
-Any time you have multiple pieces of data, and you need to compare them. 
-- anagrams
-- one array is equal to another array squared each individual piece 
-- to tell if numbers consist of the same digits just in a different order
-*/
-
 function validAnagram(first, second) {
-  if (first.length !== second.length) {
-    return false;
-  }
-
-  const lookup = {};
-
-  for (let i = 0; i < first.length; i++) {
-    let letter = first[i];
-    // if letter exists, increment, otherwise set to 1
-    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
-  }
-  console.log(lookup)
-
-  for (let i = 0; i < second.length; i++) {
-    let letter = second[i];
-    // can't find letter or letter is zero then it's not an anagram
-    if (!lookup[letter]) {
-      return false;
-    } else {
-      lookup[letter] -= 1;
+    if (first.length !== second.length) {
+        return false;
     }
-  }
 
-  return true;
+    const lookup = {};
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i];
+        // if letter exists, increment, otherwise set to 1
+        lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+    }
+    console.log(lookup);
+
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i];
+        // can't find letter or letter is zero then it's not an anagram
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+
+    return true;
 }
 
 // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
-validAnagram('anagrams', 'nagaramm')
+validAnagram('anagrams', 'nagaramm');
